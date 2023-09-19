55672ad62ed824bf15c7c22eee1ac94bbbd15778
isha988 <ishakapoor.988@gmail.com>
2023-09-19 12:53:40 +0530

    designed pie chart

--- src/components/Dashboard/PieChart.jsx
+++ src/components/Dashboard/PieChart.jsx
@@ -7,8 +7,7 @@ import { Card } from "../UI";
 export const PieChart = ({ className }) => {
   const backgroundColor = ["#98d89e", "#ee8484", "#f6dc7d", "#8a94e0"];
   const [data, setData] = useState({
-    labels: ["a", "s", "d"],
-    values: [12, 50, 40],
+    backgroundColor,
   });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -18,6 +17,7 @@ export const PieChart = ({ className }) => {
       setIsLoading(true);
       const response = await pieChart();
       setData({
+        ...data,
         labels: Object.keys(response),
         values: Object.values(response),
       });
@@ -33,28 +33,53 @@ export const PieChart = ({ className }) => {
   // }, []);
 
   return (
-    <Card className={className}>
-      <Doughnut
-        type="doughnut"
-        data={{
-          labels: data?.labels,
-          datasets: [
-            {
-              data: data?.values,
-              backgroundColor: backgroundColor,
-              hoverOffset: 4,
-            },
-          ],
-        }}
-        options={{
-          cutout: "80%",
-          plugins: {
-            legend: {
-              display: false,
-            },
-          },
-        }}
-      />
+    <Card className={`p-6 px-8 rpunded-xl ${className}`}>
+      {/* header */}
+      <div className="flex flex-wrap mb-8 items-center justify-between gap-x-4 gap-y-1">
+        <h2 className="font-bold text-lg">Activities</h2>
+        <p className="text-text-100 text-sm font-montserrat">May - June 2021</p>
+      </div>
+      {/* charr\t */}
+      <div className="flex flex-wrap items-center justify-between gap-6">
+        <div className="max-h-[200px] flex-1">
+          <Doughnut
+            type="doughnut"
+            data={{
+              labels: data?.labels,
+              datasets: [
+                {
+                  data: data?.values,
+                  backgroundColor: backgroundColor,
+                  hoverOffset: 4,
+                },
+              ],
+            }}
+            options={{
+              cutout: "80%",
+              plugins: {
+                legend: {
+                  display: false,
+                },
+              },
+            }}
+          />
+        </div>
+        {/*  labels */}
+        <div className="flex-1 flex flex-wrap gap-2">
+          {data?.labels?.map((label, index) => {
+            const bg = `bg-[${data.backgroundColor[index]}]`;
+            return (
+              <div className="flex gap-3 min-w-[70px]">
+                <span className={`inline-block w-3 h-3 rounded-full ${bg}`} />
+                <div>
+                  <p className="font-bold text-sm mb-1">{label}</p>
+                  <p className="text-xs text-text-100">{data.values[index]}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Card>
   );
 };
